4cbdc30f55c31aec0196e2976e6a91a213dad7e2
coloro clororo <coloro@MacBook-Air-de-coloro.local>
2019-01-07 15:05:14 -0300

    bullet

--- src/objects/foes/Lap.gd
+++ src/objects/foes/Lap.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-export (Vector2) var speed = Vector2(40, 40)
-export (float) var deltaLerp = 0.01
+export (Vector2) var speed = Vector2(40, 80)
+export (float) var deltaLerp = 0.4
 export (int) var yDirection = -1
 
 var direction = 1
 var t = 0
 
 func _process(delta):
-	t += deltaLerp
+	t += deltaLerp * delta
 	if t > 1:
 		t = 0
 		direction *= -1
@@ -22,4 +22,4 @@ func _process(delta):
 
 func _on_Area2D_body_entered(body):
 	if body.has_method("take_damage"):
-		body.take_damage(1)
+		body.take_damage(1, Vector2(direction, 0))
