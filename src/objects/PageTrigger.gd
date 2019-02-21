75ce6638b16d99befaebc6245e9fceaa2045d044
ocio28 <ocio28@gmail.com>
2019-02-20 23:33:48 -0300

    page

---
+++ src/objects/PageTrigger.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var available = false
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("attack") and available:
+		$Page.visible = !$Page.visible
+
+
+func _on_Area2D_body_entered(body):
+	available = true
+
+
+func _on_Area2D_body_exited(body):
+	available = false
